bdb579cf5e58223d914d8d4b2ca7aafba1998e2f
LeoHongyi <hongyi@hongyis-MacBook-Pro.local>
2016-03-12 01:53:47 -0500

    Initial Commit

---
+++ CallBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallBus
+//
+//  Created by hongyi liu on 16/3/12.
+//  Copyright (c) 2016年 hongyi liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CallBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallBus
+//
+//  Created by hongyi liu on 16/3/12.
+//  Copyright (c) 2016年 hongyi liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
